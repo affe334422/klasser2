44fedb294ba68833f11b8e567e2b046af8fc298d
affe334422 <alle07002@utb.vaxjo.se>
2025-01-20 08:16:22 +0100

    ll

--- obj/Debug/net8.0/klasser2.AssemblyInfo.cs
+++ obj/Debug/net8.0/klasser2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("klasser2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3b987f66835c70393a403f000001633725a1e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2613da786869e23f88852c9a3548b397bb98791")]
 [assembly: System.Reflection.AssemblyProductAttribute("klasser2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("klasser2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
